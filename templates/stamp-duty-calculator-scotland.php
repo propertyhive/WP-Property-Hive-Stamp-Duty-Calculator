775f2c0fc2ecf2ad533b2ba94bcb6b4b8a904fac
Steve Marks <info@wp-property-hive.com>
2024-12-12 13:54:55 +0000

    Add escaping of attributes to templates

--- templates/stamp-duty-calculator-scotland.php
+++ templates/stamp-duty-calculator-scotland.php
@@ -1,7 +1,7 @@
 <div class="stamp-duty-calculator-scotland">
 
     <label><?php echo __( 'Purchase Price', 'propertyhive' ); ?> (&pound;)</label>
-    <input type="text" name="purchase_price" value="<?php echo $atts['price']; ?>" placeholder="e.g. 500,000">
+    <input type="text" name="purchase_price" value="<?php echo esc_attr($atts['price']); ?>" placeholder="e.g. 500,000">
 
     <label><input type="checkbox" name="ftb_scotland" id="ftb_scotland" value="1"> I'm a first time buyer</label>
     <label><input type="checkbox" name="btl_second_scotland" id="btl_second_scotland" value="1"> Property is a buy-to-let or second home</label>
